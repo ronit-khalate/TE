853a78126e9171c9f8601f787e7210209765961e
rk <ronit.khalate.comp.2020>
2023-03-14 21:13:18 -0700

    ds&bda assignment 4 added pdf included

---
+++ DS&bda/28-ronit-ass4/calculateMean.java
@@ -0,0 +1,45 @@
+import java.io.*;
+
+public class calculateMean {
+ public static void main(String[] args)
+ {
+    BufferedReader br = null;
+    String line = "";
+    String splitBy = ",";
+    int sum1=0,sum2=0,sum3=0,count=1;
+    try
+    {
+        br = new BufferedReader(new FileReader("C:\\ronit\\DS&bda\\28-ronit-ass4\\Dataset (1).csv"));
+        br.readLine();
+        try
+        {
+            while ((line = br.readLine()) != null)
+            {
+                String[] city = line.split(splitBy);
+                System.out.println("City [Name=" + city[0] + ", Temperature=" + city[1] + ", Dew_Point=" +
+                city[2] + ", Wind=" + city[3] );
+                String[] country = line.split(",");
+                int dew=Integer.parseInt(country[2]);
+                int temp=Integer.parseInt(country[1]);
+                int wind=Integer.parseInt(country[2]);
+                sum1=sum1+temp;
+                sum2=sum2+dew;
+                sum3=sum3+wind;
+                count++;
+            }
+        }
+        catch (NumberFormatException | IOException e)
+        {
+            System.out.println("NA"); e.printStackTrace();
+        }
+    }
+    catch (Exception e)
+    {
+        e.printStackTrace();
+    }
+
+    System.out.println("mean of Temperature ="+sum1/count);
+    System.out.println("mean of Dew point="+sum2/count);
+    System.out.println("mean of Wind ="+sum3/count);
+    }
+}
---
+++ DS&bda/28-ronit-ass4/readCsv.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class readCsv {
+ public static void main(String args[]){
+
+        String line="";
+        String splitBy=",";
+        try{
+            BufferedReader br= new BufferedReader(new
+            FileReader("C:\\ronit\\DS&bda\\28-ronit-ass4\\Dataset (1).csv"));
+            while((line = br.readLine()) !=null){
+            String [] country = line.split(splitBy);
+            System.out.println("Country [Name= "+country[0] + ", Tempreture=" +country[1] +"Dew_point="+country[2] + ", Wind=" + country[3] );
+             }      
+        }
+        catch (IOException e){
+        e.printStackTrace();
+        }
+    }
+}
